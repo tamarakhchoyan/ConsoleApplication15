efb796de41c005cd3383cfdab6cfb99d3b993e0c
User <User@DESKTOP-9TGTEUB>
2023-11-28 02:35:06 +0400

    Add project files.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+	int a;
+	int b;
+	std:: cin >> a;
+	std::cin >> b;
+	int s = a + b;
+	int p = a - b;
+	int m = a * b;
+	std::cout << s << std::endl;
+	std::cout << p << std::endl;
+	std::cout << m << std::endl;
+	return 0;
+}
